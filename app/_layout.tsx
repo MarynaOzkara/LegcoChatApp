c0a4bb0ab05c3d7d2dae8be7cb30cc477d0b2367
MarynaOzkara <ozkaramaryna@gmail.com>
2025-06-05 22:08:59 +0300

    add chat image

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
-import { Stack } from "expo-router";
-import "../global.css"
+import { ThemeProvider, useTheme } from "@/context/theme-context";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "../global.css";
+
+
+function Layout() {
+  const { isDark } = useTheme();
+  return (<><Slot/><StatusBar style = {isDark ? "light" : "dark"}/></>)
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <ThemeProvider>
+    <Layout/>
+    
+  </ThemeProvider>) 
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
-import { Text, View } from "react-native";
+import { useTheme } from "@/context/theme-context";
+import { Image, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const {isDark} = useTheme()
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      className="bg-red-500"
-    >
-      <Text className="text-white text-5xl font-bold">Welcome to Legco Chat</Text>
-    </View>
+    <SafeAreaView className={`flex-1 ${isDark ? "bg-[#1a1c1e]" : "bg-white"}`}>
+      <View className="flex-1">
+        <View className="items-center mt-20" >
+          <Image source={require("@/assets/images/chat.png")} style={{width: 200, height: 200, marginBottom: 20}}/>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
